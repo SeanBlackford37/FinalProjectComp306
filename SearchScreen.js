d39310a08f2acd8a1c18a33ca96afbab68ffd0c9
tjones4261 <73116929+tjones4261@users.noreply.github.com>
2020-12-06 18:11:46 -0500

    broken seacrh system

--- SearchScreen.js
+++ SearchScreen.js
@@ -12,16 +12,18 @@ export function SearchScreen({ navigation }) {
         { key: "2", name: "Rocket League", avgScore: 7, avgPT: 250 },
         { key: "3", name: "God Of War", avgScore: 8.7, avgPT: 20 },
         { key: "4", name: "Spider-Man: Miles Morales", avgScore: 9.3, avgPT: 10 },
-        { key: "5", name: "Minecraft", avgScore: 8.8, avgPT: 1000 },
-        { key: "6", name: "Rocket League", avgScore: 7, avgPT: 250 },
-        { key: "7", name: "God Of War", avgScore: 8.7, avgPT: 20 },
-        { key: "8", name: "Spider-Man: Miles Morales", avgScore: 9.3, avgPT: 10 },
+        { key: "5", name: "Minecraft: Dungeons", avgScore: 8.8, avgPT: 1000 },
+        { key: "6", name: "Spiderman", avgScore: 7, avgPT: 250 },
+        { key: "7", name: "Dark Souls", avgScore: 8.7, avgPT: 20 },
+        { key: "8", name: "Mad Max", avgScore: 9.3, avgPT: 10 },
     ];
 
     let [result, setResult] = useState(theData);
     let [searchTitle, setSearchTitle] = useState("null");
     let [searchMinScore, setSearchMinScore] = useState(0);
     let [searchMinPT, setSearchMinPT] = useState(0);
+
+    let [tempTitle, setTempTitle] = useState("null");
 
     const _renderItem = input => (
 
@@ -37,6 +39,30 @@ export function SearchScreen({ navigation }) {
 
     const _search = () => {
         console.log("searching");
+        setResult(theData);
+
+        if (searchTitle != "null" && searchTitle != "") {
+            //setResult(result.filter(checkTitle));
+        }
+
+        setResult(result.filter(checkAboveScore));
+
+        setResult(result.filter(checkAbovePT));
+    }
+
+    const checkTitle = (item) => {
+        
+        //setTempTitle(item.title);
+        //console.log(tempTitle.substring(0, searchTitle.length) + " and " + searchTitle);
+        //return tempTitle.substring(0, searchTitle.length) == searchTitle.toLowerCase() || tempTitle.substring(0, searchTitle.length) == searchTitle;
+    }
+
+    const checkAboveScore = (item) => {
+        return item.avgScore >= setSearchMinScore;
+    }
+
+    const checkAbovePT = (item) => {
+        return item.avgPT >= setSearchMinPT;
     }
 
     return (
