306bd40d478de898ab031faf7b98e3a82e519d9b
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2020-12-08 11:07:51 -0500

    Moved import

--- ProfileScreen.js
+++ ProfileScreen.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import { Button, Text, View, FlatList, TouchableHighlight, 
-	TouchableOpacity, Alert, Image, Modal, TextInput } from 'react-native';
+import { Button, Text, View, FlatList, TouchableHighlight, TouchableOpacity, Alert, Image, Modal, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
