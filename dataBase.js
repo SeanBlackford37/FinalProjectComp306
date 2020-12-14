9c0dba5710119208f4c4d2d266e46129e5dc85c0
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2020-12-14 16:40:01 -0500

    Added fake data to the database

--- dataBase.js
+++ dataBase.js
@@ -8,7 +8,22 @@ const INITIAL_STATE = {
   possible: [],
   
 };
-
+const randId = () => {
+  var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var ID = '';
+      for (var j = 0; j < 10; j++ ) {
+          ID += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+      }
+      return ID
+}
+//Creates a random numbers of posts to fill content
+var numOfPosts = Math.floor(Math.random() * 40) + 10;
+for(var i = 0; i < numOfPosts; i++){
+   var randomID = randId()
+   let game = DATA[Math.floor(Math.random() * DATA.length)]
+   DATA.push({PostID: randomID, profilePic: "https://miro.medium.com/max/10600/1*laxNFhX8J5J7LHcBkj8CiA.jpeg", firstPost: "False", gameName: game.gameName , playTime: Math.floor(Math.random() * 1500) + 25, userScore: Math.floor(Math.random() * 10) + 1,  postContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', picture: game.picture, added:"no"});
+    
+}
 const gameReviews = (state = INITIAL_STATE, action)  => {
   const {
     current,
@@ -19,17 +34,16 @@ const gameReviews = (state = INITIAL_STATE, action)  => {
     case 'ADD_GAME':
       var count = 0
       //Creates a unique key ID
-      var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      var ID = '';
-      for (var j = 0; j < 10; j++ ) {
-          ID += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
-      }
+      var ID = randId()
       for (var i = 0; i < current.length;i++){
         if(current[i].gameName == action.gameName){
           
           count++;
         }
       }
+      
+
+
       if(count == 0){ //Adds the games to the list of games if one is not found
         current.push({PostID: ID, profilePic: "https://static.wikia.nocookie.net/xbox/images/d/d6/Orange-black-skull.jpg/revision/latest/top-crop/width/300/height/300?cb=20200426103335", firstPost: "True", gameName: action.gameName, playTime: action.playTime, userScore: action.score,  postContent: action.review, picture:"https://images-na.ssl-images-amazon.com/images/I/91ZmgFvglpL.png", added: "no", avgScore: 0, avgPlayTime: 0, numberOfReviews: 0,});
         current.push({PostID: ID + "7", profilePic: "https://static.wikia.nocookie.net/xbox/images/d/d6/Orange-black-skull.jpg/revision/latest/top-crop/width/300/height/300?cb=20200426103335", firstPost: "False", gameName: "Mario Cart", playTime: action.playTime, userScore: action.score,  postContent: action.review, picture:"https://images-na.ssl-images-amazon.com/images/I/91ZmgFvglpL.png", added: "no"});
